e989b04954290bb111e569c4aeabd20140e720d0
Jiw Surachat <jiw.surachat@gmail.com>
2020-02-06 16:54:51 +0700

    Test deploy

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ else
 </head>
 <body>
     <section class="congratulations">
-        <h1>Congratulations! This is Awesome!</h1>
+        <h1>Test Application 1!</h1>
         <p>My AWS Elastic Beanstalk <em>PHP</em> application is now running on my own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
     </section>
